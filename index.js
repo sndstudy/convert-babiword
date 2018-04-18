f9bc34110c1219d8f91df3f2bb73280000867175
sndstudy <sanada2054tech@gmail.com>
2018-04-18 23:35:33 +0900

    rewireでprivate関数のテストを変更

---
+++ index.js
@@ -0,0 +1,13 @@
+function stringSlice(targetStr){
+
+    var arrayChar = [];
+
+    for(var i=0; i<targetStr.length;i++){
+        
+        arrayChar.push(targetStr.charAt(i));
+
+    }
+
+    return arrayChar;
+    
+};
--- spec/testSpec.js
+++ spec/testSpec.js
@@ -1,8 +1,10 @@
-var stringSlice = require("../stringSlice.js");
+var rewire = require("rewire");
+var index = rewire("../index.js")
 
 describe("文字列分割テスト",function(){
 
     it("ひらがなのみ",function(){
+        var stringSlice = index.__get__("stringSlice");
         expect(stringSlice("あいうえお").length).toBe(5);
     });
 
