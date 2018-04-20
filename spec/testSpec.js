557e8517abd88562d14c3ef2fba0716dfe6247e2
sndstudy <sanada2054tech@gmail.com>
2018-04-20 22:16:05 +0900

    母音がaの場合の検証パターン追加

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -24,6 +24,20 @@ describe("母音テスト",function(){
     it("母音がaの場合",function(){
 
         expect(getBabiChar("あ")).toBe("ば");
+        expect(getBabiChar("か")).toBe("ば");
+        expect(getBabiChar("さ")).toBe("ば");
+        expect(getBabiChar("た")).toBe("ば");
+        expect(getBabiChar("な")).toBe("ば");
+        expect(getBabiChar("は")).toBe("ば");
+        expect(getBabiChar("ま")).toBe("ば");
+        expect(getBabiChar("や")).toBe("ば");
+        expect(getBabiChar("ら")).toBe("ば");
+        expect(getBabiChar("わ")).toBe("ば");
+        expect(getBabiChar("が")).toBe("ば");
+        expect(getBabiChar("ざ")).toBe("ば");
+        expect(getBabiChar("だ")).toBe("ば");
+        expect(getBabiChar("ば")).toBe("ば");
+        expect(getBabiChar("ぱ")).toBe("ば");
 
     })
 
